d89ccce3b9336fae991116d423884c092ba030fa
RoyceLuo1216 <royceluo1216@gmail.com>
2023-11-05 11:04:55 -0500

    Frontend

---
+++ frotned.java
@@ -0,0 +1,480 @@
+Treehard
+treehqrd
+😭losing it
+team diegression
+
+mondayleftmebroken, — Yesterday at 2:03 PM
+W
+hugeturnip — Yesterday at 2:03 PM
+HI TEAM
+Treehard
+ changed the channel name: 
+THE WINNERS
+ — Yesterday at 2:03 PM
+Treehard
+ changed the channel name: 
+WINNERS POV
+ — Yesterday at 2:03 PM
+Treehard — Yesterday at 2:13 PM
+https://github.com/noelogbuagu/Toronto-House-Price-Prediction?source=post_page-----948d0cf72f58--------------------------------
+GitHub
+GitHub - noelogbuagu/Toronto-House-Price-Prediction
+Contribute to noelogbuagu/Toronto-House-Price-Prediction development by creating an account on GitHub.
+GitHub - noelogbuagu/Toronto-House-Price-Prediction
+https://github.com/noelogbuagu/Webscraping_Realtor.ca/blob/main/get_data.ipynb
+GitHub
+Webscraping_Realtor.ca/get_data.ipynb at main · noelogbuagu/Webscra...
+Contribute to noelogbuagu/Webscraping_Realtor.ca development by creating an account on GitHub.
+Webscraping_Realtor.ca/get_data.ipynb at main · noelogbuagu/Webscra...
+mondayleftmebroken, — Yesterday at 2:23 PM
+Attachment file type: acrobat
+midterm1_2015.pdf
+967.77 KB
+Attachment file type: acrobat
+midterm1_2016.pdf
+1.13 MB
+Attachment file type: acrobat
+midterm1_2014.pdf
+855.86 KB
+mondayleftmebroken, — Yesterday at 3:58 PM
+@hugeturnip @BigManFrankie357 yalls coming back or going to next workshop
+mondayleftmebroken, — Yesterday at 4:21 PM
+https://github.com/shanuhalli/Project-Real-Estate-Price-Prediction/tree/main
+GitHub
+GitHub - shanuhalli/Project-Real-Estate-Price-Prediction: Worked on...
+Worked on Real Estate Price data analysis by scraping website from www.99acres.com to help the housing domain as well as estimate the sale value of various houses and open plots. - GitHub - shanuha...
+GitHub - shanuhalli/Project-Real-Estate-Price-Prediction: Worked on...
+mondayleftmebroken, — Yesterday at 4:27 PM
+mumbai model from here has these features:
+
+Property_Name    Location    Region    Property_Age    Availability    Area_Tpye    Area_SqFt    Rate_SqFt    Floor_No    Bedroom    Bathroom    Price_Lakh
+mondayleftmebroken, — Yesterday at 4:28 PM
+toronto model (bad model but good dataset) has these features:
+mls_number    price    address    above_grade_bedrooms    below_grade_bedrooms    bathrooms    community_name    land_size    annual_prop_tax
+common features:
+price
+land size
+#of bedrooms
+#of bathrooms
+address 
+repo link: https://github.com/chriskrunchy/Real-Estate-Value-Predictor.git
+GitHub
+GitHub - chriskrunchy/Real-Estate-Value-Predictor
+Contribute to chriskrunchy/Real-Estate-Value-Predictor development by creating an account on GitHub.
+GitHub - chriskrunchy/Real-Estate-Value-Predictor
+mondayleftmebroken, — Yesterday at 7:27 PM
+https://github.com/slavaspirin/Toronto-housing-price-prediction
+GitHub
+GitHub - slavaspirin/Toronto-housing-price-prediction: Building Tor...
+Building Toronto Housing dataset from scratch to predict real estate prices - GitHub - slavaspirin/Toronto-housing-price-prediction: Building Toronto Housing dataset from scratch to predict real es...
+GitHub - slavaspirin/Toronto-housing-price-prediction: Building Tor...
+Treehard — Yesterday at 7:38 PM
+https://medium.com/@slavaspirin/toronto-housing-price-prediction-359b4a0aa9a7
+Medium
+Toronto Housing Price Prediction
+The volume of preliminary work this project required amounted to perhaps one of the most challenging yet rewarding experiences I’ve had…
+Toronto Housing Price Prediction
+Treehard — Yesterday at 7:52 PM
+https://www.kaggle.com/datasets/camnugent/california-housing-prices
+California Housing Prices
+Median house prices for California districts derived from the 1990 census.
+Image
+hugeturnip
+ changed the channel name: 
+team diegression
+ — Yesterday at 8:30 PM
+Treehard — Yesterday at 8:46 PM
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+# Example data preparation code
+X = your_features  # Feature matrix
+y = your_target    # Target variable
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+mondayleftmebroken, — Yesterday at 8:49 PM
+i almost tripped at number of digits
+Image
+mondayleftmebroken, — Yesterday at 10:18 PM
+https://leanscape.io/the-box-cox-transformation-what-it-is-and-how-to-use-it/#:~:text=What%20is%20the%20Box%20Cox,predictions%20made%20using%20linear%20regression.
+LeanScape
+Reagan Pannell
+The Box-Cox Transformation: What It Is and How to Use It - LeanScape
+Box Cox TransformationThe Box Cox Transformation is a popular method of transforming non-normal dependent variables into a normal shape. This technique help ...
+The Box-Cox Transformation: What It Is and How to Use It - LeanScape
+hugeturnip — Yesterday at 10:35 PM
+https://colab.research.google.com/drive/1R1egE_mGpumq8ojRHryjbbYrrDLJ_Zsj?usp=sharing
+Google Colaboratory
+Image
+Treehard — Yesterday at 10:35 PM
+from sklearn.preprocessing import LabelEncoder
+mondayleftmebroken, — Yesterday at 10:56 PM
+df.to_csv('file.csv', index=False)
+Treehard — Today at 1:53 AM
+43.6701° N, 79.3862° W
+hugeturnip — Today at 1:28 AM
+Image
+43.64698170000000, -79.5303556
+BigManFrankie357 — Today at 1:51 AM
+43.905626,-79.449659
+Treehard — Today at 2:06 AM
+Bridle Path-Sunnybrook-York Mills, Toronto, ON
+Treehard — Today at 9:13 AM
+xhttps://devpost.com/software/ecolink-tech
+Devpost
+EcoLink Tech
+Visualize the impact and supply chain complexity of an electronic device.
+Image
+hugeturnip — Today at 9:38 AM
+http://127.0.0.1:5000/predit
+http://138.51.68.255:5000/predit 
+hugeturnip — Today at 9:52 AM
+https://f951-138-51-68-255.ngrok-free.app/predit
+hugeturnip — Today at 10:06 AM
+https://chat.openai.com/share/76aa8379-2d0e-480d-abf3-7308c8f7b448
+ChatGPT
+ChatGPT
+Shared via ChatGPT
+Image
+mondayleftmebroken, — Today at 10:16 AM
+shit i just woke up
+gimme like 10 mins
+BigManFrankie357 — Today at 10:21 AM
+Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl("https://f951-138-51-68-255.ngrok-free.app/predit")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+            ApiService apiService = retrofit.create(ApiService.class);
+
+            Call<Data> call = apiService.getUser();
+            call.enqueue(new Callback<Data>() {
+                @Override
+                public void onResponse(Call<Data> call, Response<Data> response) {
+                    if (response.isSuccessful()) {
+                        Data user = response.body();
+                        // Do something with the user object
+                    } else {
+                        // Handle the error
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<Data> call, Throwable t) {
+                    // Handle the failure
+                }
+            });
+hugeturnip — Today at 10:23 AM
+https://chat.openai.com/share/76aa8379-2d0e-480d-abf3-7308c8f7b448
+ChatGPT
+ChatGPT
+Shared via ChatGPT
+Image
+https://chat.openai.com/share/76aa8379-2d0e-480d-abf3-7308c8f7b448
+ChatGPT
+ChatGPT
+Shared via ChatGPT
+Image
+UserData userData = new UserData();
+userData.setName("John Doe");
+userData.setEmail("johndoe@example.com");
+
+ApiService apiService = retrofit.create(ApiService.class);
+Call<UserResponse> call = apiService.createUser(userData);
+
+call.enqueue(new Callback<UserResponse>() {
+    @Override
+    public void onResponse(Call<UserResponse> call, Response<UserResponse> response) {
+        if (response.isSuccessful()) {
+            UserResponse userResponse = response.body();
+            // Handle the received data
+            int id = userResponse.getId();
+            String message = userResponse.getMessage();
+        } else {
+            // Handle the error. You can also extract more info from the 'response' object if needed.
+        }
+    }
+
+    @Override
+    public void onFailure(Call<UserResponse> call, Throwable t) {
+        // Handle the failure
+    }
+});
+43.905626
+{
+    "list_price": 99999,
+    "bathrooms": 4,
+    "sqft": 2250,
+    "lat": 43.905626,
+    "long": -79.449659,
+    "mean_district_income": 70600,
+    "bedrooms_ag": 2,
+    "bedrooms_bg": 1
+}
+hugeturnip — Today at 10:35 AM
+public interface ApiService {
+    @POST("users/create")
+    Call<UserResponse> createUser(@Body UserData userData);
+}
+https://d3a6-138-51-68-255.ngrok-free.app/
+hugeturnip — Today at 10:42 AM
+public interface ApiService {
+    
+    @GET("users/{userId}")
+    Call<UserResponse> getUser(@Path("userId") int userId);
+    
+    @POST("users/create")
+    Call<CreateUserResponse> createUser(@Body UserData userData);
+}
+Treehard — Today at 10:57 AM
+royceluo1216@gmail.com
+BigManFrankie357 — Today at 10:59 AM
+Attachment file type: archive
+RealTor.zip
+22 bytes
+package com.example.t;
+
+import static java.lang.Math.round;
+
+import androidx.fragment.app.FragmentActivity;
+
+Expand
+message.txt
+10 KB
+﻿
+package com.example.t;
+
+import static java.lang.Math.round;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Bundle;
+import android.service.autofill.UserData;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.SearchView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.t.databinding.ActivityMapsBinding;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private  LatLng latLng = new LatLng(0.0, 0.0);
+    private Integer radius = 500;
+    private ActivityMapsBinding binding;
+    private SearchView mapSearchView;
+
+    public class Data {
+        public double list_price;
+        public int bathrooms;
+        public double sqft;
+        public double lat;
+        public double lon;
+
+        public double mean_district_income;
+        public int bedrooms_ag;
+        public int bedrooms_bg;
+
+
+    }
+
+    public class Price{
+        private int predicted_price;
+    }
+    public interface ApiService {
+
+        @GET("predit")
+        Call<Price> getUser(@Path("userId") int userId);
+
+        @POST("predit")
+        Call<Price> createUser(@Body Data userData);
+    }
+    private Button nextButton;
+
+    private ArrayList rawUserData;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        nextButton = (Button) findViewById(R.id.next_button);
+
+        nextButton.setOnClickListener(this::onClick);
+
+        nextButton.setVisibility(View.INVISIBLE);
+        mapSearchView = findViewById(R.id.mapSearch);
+        mapSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                String location = mapSearchView.getQuery().toString();
+                List<Address> addressList = null;
+
+                if (location != null){
+                    Geocoder geocoder = new Geocoder(MapsActivity.this);
+
+                    try{
+                        addressList = geocoder.getFromLocationName(location, 1);
+
+                    } catch (IOException e){
+                        e.printStackTrace();
+                    }
+
+                    Address address = addressList.get(0);
+
+                    latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+                    CircleOptions circly = new CircleOptions().center(latLng).radius(500);
+                    Circle circle = mMap.addCircle(circly);
+                    circle.setFillColor(Color.argb(50, 200, 10, 10));
+                    circle.setStrokeColor(Color.RED);
+                    nextButton.setVisibility(View.VISIBLE);
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+
+        mapFragment.getMapAsync(this);
+
+
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.address_and_spinner_LinearLayout);
+        linearLayout.bringToFront();
+        Spinner spinner = findViewById(R.id.radius_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.radius_array,
+                android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (latLng.latitude != 0.0 && latLng.longitude != 0.0) {
+                    TextView radiusSelected = findViewById(view.getId());
+                    String radiusValue = radiusSelected.getText().toString();
+                    radius = Integer.parseInt(radiusValue.substring(0, radiusValue.length()-1));
+                    mMap.clear();
+                    mMap.addMarker(new MarkerOptions().position(latLng));
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+                    CircleOptions circly = new CircleOptions().center(latLng).radius(radius);
+                    Circle circle = mMap.addCircle(circly);
+                    circle.setFillColor(Color.argb(50, 200, 10, 10));
+                    circle.setStrokeColor(Color.RED);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+    }
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+    }
+
+    public void onClick(View v)
+    {
+        if (v == nextButton)
+        {
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl("https://d3a6-138-51-68-255.ngrok-free.app/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+            ApiService apiService = retrofit.create(ApiService.class);
+
+            Data userData = new Data();
+            EditText price = (EditText) findViewById(R.id.price);
+            EditText washrooms = (EditText) findViewById(R.id.num_washrooms);
+            EditText bathrooms = (EditText) findViewById(R.id.bathrooms);
+            EditText price = (EditText) findViewById(R.id.price);
+            EditText price = (EditText) findViewById(R.id.price);
+            userData.list_price = Integer.parseInt(String.valueOf(price.getText()));
+            userData.bathrooms = Integer.parseInt(String.valueOf(washrooms.getText()));
+            userData.sqft = 2250;
+            userData.lat = 43.905626;
+            userData.lon = -79.449659;
+            userData.mean_district_income = 70600;
+            userData.bedrooms_ag = 2;
+            userData.bedrooms_bg = 1;
+
+            Call<Price> call = apiService.createUser(userData);
+
+            call.enqueue(new Callback<Price>() {
+                @Override
+                public void onResponse(Call<Price> call, Response<Price> response) {
+                    if (response.isSuccessful()) {
+                        Price userResponse = response.body();
+                        // Handle the received data
+                        int id = userResponse.predicted_price;
+                        String returnedPrice = Integer.toString(id);
+                    } else {
+                        // Handle the error. You can also extract more info from the 'response' object if needed.
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<Price> call, Throwable t) {
+                    // Handle the failure
+                }
+            });
+
+        }
+        return;
+
+    }
+
+
+
+}
+message.txt
+10 KB
